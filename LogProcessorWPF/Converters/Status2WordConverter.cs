27fa63bdd0fa3c134111d34ed6229837625e6f95
Lei <yang.lei@intel.com>
2016-08-16 18:28:53 +0800

    Grouped test by status

---
+++ LogProcessorWPF/Converters/Status2WordConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using LogProcessor;
+using System.Windows.Media;
+namespace LogProcessorWPF.Converters
+{
+    [ValueConversion(typeof(string), typeof(Brush))]
+    public class Status2WordConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+            switch (s)
+            {
+                case Constants.passCharString:
+                    return "Pass";
+                case Constants.failCharString:
+                    return "Fail";
+
+                case Constants.errorCharString:
+                    return "Error";
+            }
+            return "Unknown Status";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
